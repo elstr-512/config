7b5a20179eb2bdecae45d9775558dc3c41d382e9
elstr-512 <122643627+elstr-512@users.noreply.github.com>
2024-12-18 14:53:35 +0000

    rm

--- nvim/lua/configs/dap.lua
+++
@@ -1,6 +0,0 @@
-local dap = require('dap')
-dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = "/home/ee/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD8",
-}
